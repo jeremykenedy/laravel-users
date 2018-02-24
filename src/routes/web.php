0d41308f459e21f8f22ffb3760fb7a34ed935f69
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2018-02-24 06:34:37 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/routes/web.php
+++ src/routes/web.php
@@ -8,7 +8,7 @@
 */
 
 // APP Routes Below
-Route::group(['middleware' => 'web', 'namespace' => 'jeremykenedy\laravelusers\app\Http\Controllers'], function () {
+Route::group(['middleware' => 'web', 'namespace' => 'jeremykenedy\laravelusers\app\Http\Controllers'], function() {
     Route::resource('users', 'UsersManagementController', [
         'names' => [
             'index'   => 'users',
